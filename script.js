711c43bbf5a2f84e57baeddbe2c3edc6aa194802
Jayrich <jrkrichards@gmail.com>
2020-10-13 03:44:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  let 
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
